acb2ad10d6a4748da6400349435618d008fd7170
Yuvraj883 <kingyuvi88@gmail.com>
2021-12-06 15:50:23 +0530

    Corrected variable names

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const app = Vue.createApp({
             inStock: true,
             detail: ['8 GB RAM', '256 GB Storage', 'A15 Bionic Chip'],
             variant:[{id:1, color:'#B76E79',image:'./assets/images/asr-1.jpg' },
-            {id:2, color:'#343d46',image:'./assets/images/asr-2.jpg' },
-            {id:3, color:'#FFFFFF',image:'./assets/images/asr-3.jpg' }
+            {id:2, color:'#FFD700',image:'./assets/images/asr-2.jpg' },
+            {id:3, color:'#343d46',image:'./assets/images/asr-3.jpg' }
         ]
 
         }
