56fa984ada290649fe6d058d726414ebd6a9c547
Yuvraj883 <kingyuvi88@gmail.com>
2021-12-07 00:02:25 +0530

    Added computed properties

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            cart :0,
-            image:'./assets/images/asr-1.jpg',
-            product : 'Mobile Phone',
-            inStock: true,
+    data() {
+        return {
+            cart: 0,
+            //image: './assets/images/asr-1.jpg',
+            brand: 'Doge',
+            product: 'Mobile Phone',
+            selectedVariant:0,
+           // inStock: false,
             detail: ['8 GB RAM', '256 GB Storage', 'A15 Bionic Chip'],
-            variant:[{id:1, color:'#B76E79',image:'./assets/images/asr-1.jpg' },
-            {id:2, color:'#FFD700',image:'./assets/images/asr-2.jpg' },
-            {id:3, color:'#343d46',image:'./assets/images/asr-3.jpg' }
-        ]
+            variant: [{ id: 1, color: '#B76E79', image: './assets/images/asr-1.jpg', quantity: 49 },
+            { id: 2, color: '#FFD700', image: './assets/images/asr-2.jpg',quantity:100 },
+            { id: 3, color: '#343d46', image: './assets/images/asr-3.jpg',quantity:0 }
+            ]
 
         }
     },
     methods: {
-        addToCart(){
-            this.cart +=1
+        addToCart() {
+            this.cart += 1
         },
-        removeFromCart(){
-            this.cart -=1
+        removeFromCart() {
+            this.cart -= 1
         },
-        updateImage(variantImage){
-this.image = variantImage
+        updateVarient(index) {
+            this.selectedVariant = index
+        }
+
+    },
+    computed: {
+        title(){
+            return this.brand+' '+this.product
+        },
+        image(){
+            return this.variant[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variant[this.selectedVariant].quantity
         }
 
     }
